015c997a6c69db732a04dec77466e73cd22c77eb
ladino2292 <56163983+ladino2292@users.noreply.github.com>
2020-04-16 21:09:57 -0500

    con_rojo

--- juego.py
+++ juego.py
@@ -14,7 +14,7 @@ class Jugador(pygame.sprite.Sprite):
     def __init__(self, pos):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface([50, 50])
-        self.image.fill(AMARILLO)
+        self.image.fill(ROJO)
         self.rect = self.image.get_rect()
         self.rect.x = pos[0]
         self.rect.y = (ALTO - self.rect.height) - 10
