fc7818282efa660c33caf4e1d16bd1e44fcbc380
ladino2292 <56163983+ladino2292@users.noreply.github.com>
2020-04-22 18:34:53 -0500

    22/04/20 sonidos y sprites,configurar

--- juego.py
+++ juego.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 
+
 ANCHO = 800
 ALTO = 600
 NEGRO = [0, 0, 0]
@@ -19,7 +20,7 @@ class Jugador(pygame.sprite.Sprite):
         self.rect.y = (ALTO - self.rect.height) - 10
         self.velx = 0
         self.vely=0
-        self.vida=0
+        self.vida=2
 
     def RetPos(self):
         x = self.rect.x
@@ -74,7 +75,7 @@ class Rival(pygame.sprite.Sprite):
 class Bala(pygame.sprite.Sprite):
     def __init__(self, pos):
         pygame.sprite.Sprite.__init__(self)
-        self.image=pygame.image.load('misil1.png')
+        self.image=pygame.image.load('bullets.png')
         self.rect = self.image.get_rect()
         self.rect.x = pos[0]
         self.rect.y = pos[1]
@@ -90,11 +91,40 @@ if __name__ == '__main__':
     pygame.init()
     # Definicion de variables
     ventana = pygame.display.set_mode([ANCHO, ALTO])
+
+    #seccion antes del juego
+    pygame.mixer.init()
+    sonido_Balas = pygame.mixer.Sound('machine_gun.ogg')
+    sonido_Misil = pygame.mixer.Sound('misil.ogg')
+    msc=pygame.mixer.Sound('Heroic_Intrusion.ogg')
+    fuente_prev=pygame.font.Font(None,46)
+    txt_titulo=fuente_prev.render('JUEGO EJEMPLO',True,BLANCO)
+    msc.play()
+
+    #carga sonidos
+
+
+
+    fin = False
+    fin_prev=False
+    while (not fin) and (not fin_prev):
+        # Gestion eventos
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                fin = True
+            if event.type == pygame.KEYDOWN:
+                fin_prev=True
+        ventana.blit(txt_titulo,[350,300])
+        pygame.display.flip()
+    msc.stop()
+
+    #Seccion del juego
     jugadores = pygame.sprite.Group()
     rivales = pygame.sprite.Group()
     balas = pygame.sprite.Group()
     balas_r=pygame.sprite.Group()
 
+    fuente_j=pygame.font.Font(None,32)
     j = Jugador([300, 200])
     jugadores.add(j)
 
@@ -111,7 +141,7 @@ if __name__ == '__main__':
     reloj = pygame.time.Clock()
     fin = False
     fin_juego=False
-    while not fin:
+    while (not fin) and (not fin_juego):
         # Gestion eventos
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -130,10 +160,19 @@ if __name__ == '__main__':
                     j.vely = 5
                     j.velx = 0
             if event.type == pygame.MOUSEBUTTONDOWN:
-                p = j.RetPos()
-                b = Bala(p)
-                b.vely = -10
-                balas.add(b)
+                if event.button==1:
+                    sonido_Balas.play()
+                    p = j.RetPos()
+                    b = Bala(p)
+                    b.vely = -10
+                    balas.add(b)
+                    reloj.tick(120)
+                    sonido_Balas.stop()
+                if event.button== 2 :
+                    sonido_Misil.play(0,2000)
+                    reloj.tick(120)
+                    sonido_Misil.stop()
+
             if event.type == pygame.KEYUP:
                 j.vely = 0
                 j.velx = 0
@@ -170,31 +209,47 @@ if __name__ == '__main__':
 
         for b in balas_r:
             ls_j=pygame.sprite.spritecollide(b,jugadores,False)
+            #elimina bala al salir de pantalla
             if b.rect.y>ALTO:
                 balas_r.remove(b)
+            contacto=True
+            #elimina bala si contacto con jugador
             for j in ls_j:
-                j.vida-=1
+                if contacto:
+                    j.vida-=1
+                    balas_r.remove(b)
+                    contacto=False
 
         for j in jugadores:
             if j.vida<0:
-                fin_juego=False
+                fin_juego=True
 
         # Refresco
-        if not fin_juego:
-            jugadores.update()
-            rivales.update()
-            balas.update()
-            balas_r.update()
-            ventana.fill(NEGRO)
-            jugadores.draw(ventana)
-            rivales.draw(ventana)
-            balas_r.draw(ventana)
-            balas.draw(ventana)
-            pygame.display.flip()
-            reloj.tick(40)
-        else:
-            fuente=pygame.front.FRONT(None,32)
-            msj=fuente.render('HAS MUERTO',True,ROJO)
-            ventana.fill(NEGRO)
-            ventana.blit(msj,[450,300])
-            pygame.display.flip()
+        mjs='vidas: '+ str(j.vida)
+        info=fuente_j.render(mjs,True,BLANCO)
+        jugadores.update()
+        rivales.update()
+        balas.update()
+        balas_r.update()
+        ventana.fill(NEGRO)
+        ventana.blit(info,[10,10])
+        jugadores.draw(ventana)
+        rivales.draw(ventana)
+        balas_r.draw(ventana)
+        balas.draw(ventana)
+        pygame.display.flip()
+        reloj.tick(40)
+
+
+
+#Después del juego
+    final = pygame.image.load('game_over.jpg')
+    while not fin:
+        # Gestion eventos
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                fin = True
+        ventana.fill(NEGRO)
+        ventana.blit(final, (0, 0))
+        pygame.display.update()
+
